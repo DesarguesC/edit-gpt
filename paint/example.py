9e0e7e942f23ff531b5336595a286b65a3bf2eba
DesarguesC <425593160@qq.com>
2023-12-10 15:32:59 +0800

    replace mask done

--- paint/example.py
+++ paint/example.py
@@ -1,4 +1,3 @@
-
 import argparse, os, sys, glob
 import cv2, torch, time, clip
 import numpy as np
@@ -29,13 +28,13 @@ safety_model_id = "CompVis/stable-diffusion-safety-checker"
 safety_feature_extractor = AutoFeatureExtractor.from_pretrained(safety_model_id)
 safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id)
 
-from ldm.util import load_model_from_config
+from pldm.util import load_model_from_config
 
 
 
-def chunk(it, size):
-    it = iter(it)
-    return iter(lambda: tuple(islice(it, size)), ())
+# def chunk(it, size):
+#     it = iter(it)
+#     return iter(lambda: tuple(islice(it, size)), ())
 
 def get_tensor_clip(normalize=True, toTensor=True):
     transform_list = []
@@ -58,31 +57,31 @@ def numpy_to_pil(images):
 
     return pil_images
 
-def put_watermark(img, wm_encoder=None):
-    if wm_encoder is not None:
-        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-        img = wm_encoder.encode(img, 'dwtDct')
-        img = Image.fromarray(img[:, :, ::-1])
-    return img
+# def put_watermark(img, wm_encoder=None):
+#     if wm_encoder is not None:
+#         img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+#         img = wm_encoder.encode(img, 'dwtDct')
+#         img = Image.fromarray(img[:, :, ::-1])
+#     return img
 
-def load_replacement(x):
-    try:
-        hwc = x.shape
-        y = Image.open("assets/rick.jpeg").convert("RGB").resize((hwc[1], hwc[0]))
-        y = (np.array(y)/255.0).astype(x.dtype)
-        assert y.shape == x.shape
-        return y
-    except Exception:
-        return x
+# def load_replacement(x):
+#     try:
+#         hwc = x.shape
+#         y = Image.open("assets/rick.jpeg").convert("RGB").resize((hwc[1], hwc[0]))
+#         y = (np.array(y)/255.0).astype(x.dtype)
+#         assert y.shape == x.shape
+#         return y
+#     except Exception:
+#         return x
 
-def check_safety(x_image):
-    safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
-    x_checked_image, has_nsfw_concept = safety_checker(images=x_image, clip_input=safety_checker_input.pixel_values)
-    assert x_checked_image.shape[0] == len(has_nsfw_concept)
-    for i in range(len(has_nsfw_concept)):
-        if has_nsfw_concept[i]:
-            x_checked_image[i] = load_replacement(x_checked_image[i])
-    return x_checked_image, has_nsfw_concept
+# def check_safety(x_image):
+#     safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
+#     x_checked_image, has_nsfw_concept = safety_checker(images=x_image, clip_input=safety_checker_input.pixel_values)
+#     assert x_checked_image.shape[0] == len(has_nsfw_concept)
+#     for i in range(len(has_nsfw_concept)):
+#         if has_nsfw_concept[i]:
+#             x_checked_image[i] = load_replacement(x_checked_image[i])
+#     return x_checked_image, has_nsfw_concept
 
 def get_tensor(normalize=True, toTensor=True):
     transform_list = []
@@ -168,15 +167,8 @@ def paint_by_example(opt, mask: torch.Tensor = None, ref_img: Image = None, base
     x_samples_ddim = model.decode_first_stage(samples_ddim)
     x_samples_ddim = torch.clamp((x_samples_ddim + 1.) / 2., min=0., max=1.)
     
-    # diffusion_inference(opt, new_target=ref_tensor, model=model,
-                            # sampler=sampler, start_code=start_code, test_model_kwargs=test_model_kwargs)
-    # cv2.imwrite(output_path, tensor2img(x_samples_ddim))
     return output_path, x_samples_ddim
 
-    # x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
-    # _ = check_safety(x_samples_ddim)
-    # x_checked_image = x_samples_ddim
-    # x_checked_image_torch = torch.from_numpy(x_checked_image).permute(0, 3, 1, 2)
 
 
 
--- pldm/util.py
+++ pldm/util.py
@@ -14,6 +14,29 @@ from inspect import isfunction
 from PIL import Image, ImageDraw, ImageFont
 
 
+
+
+def load_model_from_config(config, ckpt, verbose=False):
+    print(f"Loading model from {ckpt}")
+    pl_sd = torch.load(ckpt, map_location="cpu")
+    if "global_step" in pl_sd:
+        print(f"Global Step: {pl_sd['global_step']}")
+    sd = pl_sd["state_dict"]
+    model = instantiate_from_config(config.model)
+    m, u = model.load_state_dict(sd, strict=False)
+    if len(m) > 0 and verbose:
+        print("missing keys:")
+        print(m)
+    if len(u) > 0 and verbose:
+        print("unexpected keys:")
+        print(u)
+
+    model.cuda()
+    model.eval()
+    return model
+
+
+
 def log_txt_as_img(wh, xc, size=10):
     # wh a tuple of (width, height)
     # xc a list of captions to plot
