62540bd94ce2ed005c337b48981705e9993b1e34
DesarguesC <425593160@qq.com>
2024-03-20 20:27:20 +0800

    remove useless

--- paint/utils.py
+++ paint/utils.py
@@ -141,7 +141,7 @@ def get_clicked_point(img_path):
 
     return last_point
 
-
+from diffusers import StableDiffusionInpaintPipeline
 def replace_img_with_sd(
         img: np.ndarray,
         mask: np.ndarray,
@@ -149,7 +149,7 @@ def replace_img_with_sd(
         step: int = 50,
         device="cuda"
 ):
-    masks.astype(np.uint8) * 255
+    mask.astype(np.uint8) * 255
     
     pipe = StableDiffusionInpaintPipeline.from_pretrained(
         "stabilityai/stable-diffusion-2-inpainting",
@@ -170,64 +170,4 @@ def replace_img_with_sd(
     return img_resized
 
 
-
-@torch.no_grad()
-def inpaint_img_with_lama(
-        img: np.ndarray,
-        mask: np.ndarray,
-        config_p: str,
-        ckpt_p: str,
-        mod=8,
-        device="cuda"
-):
-    # assert len(mask.shape) == 2
-    if np.max(mask) == 1:
-        mask = mask * 255
-    masks.astype(np.uint8)    
-    
-    img = torch.from_numpy(img).float().div(255.)
-    mask = torch.from_numpy(mask).float()
-    predict_config = OmegaConf.load(config_p)
-    predict_config.model.path = ckpt_p
-    # device = torch.device(predict_config.device)
-    device = torch.device(device)
-
-    train_config_path = os.path.join(
-        predict_config.model.path, 'config.yaml')
-
-    with open(train_config_path, 'r') as f:
-        train_config = OmegaConf.create(yaml.safe_load(f))
-
-    train_config.training_model.predict_only = True
-    train_config.visualizer.kind = 'noop'
-
-    checkpoint_path = os.path.join(
-        predict_config.model.path, 'models',
-        predict_config.model.checkpoint
-    )
-    model = load_checkpoint(
-        train_config, checkpoint_path, strict=False, map_location='cpu')
-    model.freeze()
-    if not predict_config.get('refine', False):
-        model.to(device)
-
-    batch = {}
-    batch['image'] = img.permute(2, 0, 1).unsqueeze(0)
-    batch['mask'] = mask[None, None]
-    unpad_to_size = [batch['image'].shape[2], batch['image'].shape[3]]
-    batch['image'] = pad_tensor_to_modulo(batch['image'], mod)
-    batch['mask'] = pad_tensor_to_modulo(batch['mask'], mod)
-    batch = move_to_device(batch, device)
-    batch['mask'] = (batch['mask'] > 0) * 1
-
-    batch = model(batch)
-    cur_res = batch[predict_config.out_key][0].permute(1, 2, 0)
-    cur_res = cur_res.detach().cpu().numpy()
-
-    if unpad_to_size is not None:
-        orig_height, orig_width = unpad_to_size
-        cur_res = cur_res[:orig_height, :orig_width]
-
-    cur_res = np.clip(cur_res * 255, 0, 255).astype('uint8')
-    return cur_res
 
